31fb24642e6b1dbc5e6f45040a1f0aa10759b4b9
emmagomedov <emmagomedov_95@inbox.ru>
2021-05-27 13:57:13 +0300

    Шлифовка

--- gui.py
+++ gui.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 from PyQt5 import QtWidgets, QtGui
-from PyQt5.QtWidgets import QFileDialog
+from PyQt5.QtWidgets import (QFileDialog, QMessageBox)
 from converter import PDFConverter
 from qdesign import Ui_MainWindow
 
@@ -12,6 +12,9 @@ class MyWindow(QtWidgets.QMainWindow):
         super(MyWindow, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.my_message = QMessageBox()
+        self.my_message.setWindowTitle('Результат')
+        self.my_message.setText('Файл успешно сконвертирован!')
 
         self.ui.pushButton.clicked.connect(self.show_file)
         self.ui.pushButton_3.clicked.connect(self.converter)
@@ -29,24 +32,30 @@ class MyWindow(QtWidgets.QMainWindow):
         self.ui.pushButton_2.setEnabled(False)
 
     def show_dialog(self):
-        fname = QFileDialog.getOpenFileName(self, 'Open File')[0]
-        name = os.path.basename(fname).split('.')[1]
+        try:
+            fname = QFileDialog.getOpenFileName(self, 'Open File')[0]
+            name = os.path.basename(fname).split('.')[1]
 
-        if name == 'pdf':
-            self.file = fname
+            if name == 'pdf':
+                self.file = fname
+        except:
+            return
 
     def show_file(self):
         self.show_dialog()
-        self.ui.label_2.setPixmap(QtGui.QPixmap('images/pdf_icon.png'))
-        self.ui.label_3.setText(os.path.basename(self.file).split('.')[0])
-        self.ui.pushButton_2.setEnabled(True)
+        if self.file:
+            self.ui.label_2.setPixmap(QtGui.QPixmap('images/pdf_icon.png'))
+            self.ui.label_3.setText(os.path.basename(self.file).split('.')[0])
+            self.ui.pushButton_2.setEnabled(True)
 
     def converter(self):
-        name = os.path.basename(self.file).split('.')[0]
-        pdf_converter = PDFConverter(self.file, name)
-        pdf_converter.convert_to_word()
-        self.file = ''
-        self.reset()
+        if self.file:
+            name = os.path.basename(self.file).split('.')[0]
+            pdf_converter = PDFConverter(self.file, name)
+            pdf_converter.convert_to_word()
+            self.file = ''
+            self.reset()
+            self.my_message.exec()
 
     def delete(self):
         self.file = ''
